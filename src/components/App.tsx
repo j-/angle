833196b31f8256d97c971211adba7aa63925aa06
j- <j@skeoh.com>
2018-03-08 13:06:08 +1100

    Add basic app styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import Viz from '../containers/Viz';
 import Rotate from '../containers/Rotate';
+import './App.css';
 
 const App: React.StatelessComponent = () => (
 	<div className="App">
-		<Viz width={200} height={200} />
-		<Rotate />
+		<div className="App-viz">
+			<Viz width={200} height={200} />
+		</div>
+		<div className="App-config">
+			<Rotate />
+		</div>
 	</div>
 );
 
